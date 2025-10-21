7f3e585e6c5447376b60d5f03729bcacc0c39aa1
Miso Kim <kimms9725@naver.com>
2025-10-22 00:11:45 +0900

    style: tap wrapper 컴포넌트 margin 수정

--- src/features/game/components/common/TabWrapper.tsx
+++ src/features/game/components/common/TabWrapper.tsx
@@ -2,7 +2,7 @@ import { Breadcrumb } from "@/features/common";
 
 export function TabWrapper({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-full flex justify-center my-20">
+    <div className="w-full flex justify-center mt-8 mb-20">
       <div className="w-full flex flex-col justify-center items-center">
         {/* 경로 */}
         <Breadcrumb />
